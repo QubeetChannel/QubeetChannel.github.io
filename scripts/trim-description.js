0297eb2981586076df580bf677ca9d472f2563e4
Qubeet <artemanikin5@gmail.com>
2024-06-08 17:23:20 +0300

    Updates

--- scripts/trim-description.js
+++ scripts/trim-description.js
@@ -1,5 +1,5 @@
 function loadTrimData() {
-    fetch('/assets/scripts/trims.json')
+    fetch('/scripts/trims.json')
         .then(response => response.json())
         .then(data => {
             const container = document.getElementById('trim-description'); 
@@ -11,7 +11,7 @@ function loadTrimData() {
                     const trimDiv = document.createElement('div');
                     trimDiv.className = 'trim';
                     trimDiv.innerHTML = `
-                        <img src="/public/assets/images/armor_trim/trim_item/${key}.png" alt="${key} Trim">
+                        <img src="/assets/images/armor_trim/trim_item/${key}.png" alt="${key} Trim">
                         <h2>${key} Trim</h2>
                         <p>${item.Description}</p>
                         <p>Chance to find: <strong>${item.Chance}</strong></p>
